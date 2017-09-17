1f56ca7a46b07ae75d31319f20b30b7f5ee9dc11
Phil Johnson <philjohnson3@gmail.com>
2017-09-17 15:55:43 -0700

    test locks on async method

--- Tests/Extensions/Cache.cs
+++ Tests/Extensions/Cache.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using SharedUtils.Tests.Objects;
 using static SharedUtils.Extensions.CacheExtensions;
+using System.Threading.Tasks;
 
 namespace SharedUtils.Tests.Extensions
 {
@@ -20,18 +21,39 @@ namespace SharedUtils.Tests.Extensions
         [TestMethod]
         public void GetFromCache()
         {
-            DictionaryCache cache = new DictionaryCache();
-            List<string> testList = new List<string>() { "aaaa", "bbbb", "cccc" };
-            const string listStringCacheKey = "alksdjf";
-            List<string> firstResult =  cache.Get<List<string>>(listStringCacheKey, () => { return testList; });
-            CollectionAssert.AreEquivalent(testList, firstResult);
+            using (DictionaryCache cache = new DictionaryCache())
+            {
+                List<string> testList = new List<string>() { "aaaa", "bbbb", "cccc" };
+                const string listStringCacheKey = "alksdjf";
+                List<string> firstResult = cache.Get<List<string>>(listStringCacheKey, () => { return testList; });
+                CollectionAssert.AreEquivalent(testList, firstResult);
 
-            List<string> secondResult = cache.Get<List<string>>(listStringCacheKey, () => { return new List<string>(); });  //point here is that the old list is cached, doesn't matter what i pass into the function
-            CollectionAssert.AreEquivalent(testList, secondResult);
+                List<string> secondResult = cache.Get<List<string>>(listStringCacheKey, () => { return new List<string>(); });  //point here is that the old list is cached, doesn't matter what i pass into the function
+                CollectionAssert.AreEquivalent(testList, secondResult);
 
-            cache.Remove(listStringCacheKey);
-            List<string> thirdResult = cache.Get<List<string>>(listStringCacheKey, () => { return new List<string>(); });
-            CollectionAssert.AreNotEquivalent(testList, thirdResult);
+                cache.Remove(listStringCacheKey);
+                List<string> thirdResult = cache.Get<List<string>>(listStringCacheKey, () => { return new List<string>(); });
+                CollectionAssert.AreNotEquivalent(testList, thirdResult);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetFromCacheAsync()
+        {
+            using (DictionaryCache cache = new DictionaryCache())
+            {
+                List<string> testList = new List<string>() { "aaaa", "bbbb", "cccc" };
+                const string listStringCacheKey = "alksdjf";
+                List<string> firstResult = await cache.GetAsync<List<string>>(listStringCacheKey, () => { return Task.FromResult(testList); });
+                CollectionAssert.AreEquivalent(testList, firstResult);
+
+                List<string> secondResult =await cache.GetAsync<List<string>>(listStringCacheKey, () => { return Task.FromResult(new List<string>()); });  //point here is that the old list is cached, doesn't matter what i pass into the function
+                CollectionAssert.AreEquivalent(testList, secondResult);
+
+                cache.Remove(listStringCacheKey);
+                List<string> thirdResult = await cache.GetAsync<List<string>>(listStringCacheKey, () => { return Task.FromResult(new List<string>()); });
+                CollectionAssert.AreNotEquivalent(testList, thirdResult);
+            }
         }
     }
 }
