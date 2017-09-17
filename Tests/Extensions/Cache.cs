beaa2c02e641739b066ff2117cebc03f9b34feee
Phil Johnson <philjohnson3@gmail.com>
2017-09-17 08:58:47 -0700

    basic testing of the first cache features

---
+++ Tests/Extensions/Cache.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SharedUtils.Tests.Objects;
+using static SharedUtils.Extensions.CacheExtensions;
+
+namespace SharedUtils.Tests.Extensions
+{
+    [TestClass]
+    public class Cache
+    {
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //
+        }
+
+        [TestMethod]
+        public void GetFromCache()
+        {
+            DictionaryCache cache = new DictionaryCache();
+            List<string> testList = new List<string>() { "aaaa", "bbbb", "cccc" };
+            const string listStringCacheKey = "alksdjf";
+            List<string> firstResult =  cache.Get<List<string>>(listStringCacheKey, () => { return testList; });
+            CollectionAssert.AreEquivalent(testList, firstResult);
+
+            List<string> secondResult = cache.Get<List<string>>(listStringCacheKey, () => { return new List<string>(); });  //point here is that the old list is cached, doesn't matter what i pass into the function
+            CollectionAssert.AreEquivalent(testList, secondResult);
+
+            cache.Remove(listStringCacheKey);
+            List<string> thirdResult = cache.Get<List<string>>(listStringCacheKey, () => { return new List<string>(); });
+            CollectionAssert.AreNotEquivalent(testList, thirdResult);
+        }
+    }
+}
---
+++ Tests/Objects/DictionaryCache.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SharedUtils.Tests.Objects
+{
+    public class DictionaryCache : SharedUtils.Interfaces.ICacheManager
+    {
+        private ReaderWriterLockSlim cacheLock = new ReaderWriterLockSlim();
+
+        public DictionaryCache()
+        {
+            tmpCache = new Dictionary<string, object>();
+        }
+
+        public bool IsCacheActive
+        {
+            get
+            {
+                return !(tmpCache == null);
+            }
+        }
+
+        private Dictionary<string, object> tmpCache;
+
+        public void Clear()
+        {
+            tmpCache.Clear();
+        }
+
+        public void Dispose()
+        {
+            tmpCache = null;
+        }
+
+        public T Get<T>(string key)
+        {
+            if (IsSet(key))
+            {
+                return (T)tmpCache[key];
+            }
+            return default(T);
+        }
+
+        public bool IsSet(string key)
+        {
+            if (tmpCache.ContainsKey(key))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Remove(string key)
+        {
+            if (IsSet(key))
+            {
+                tmpCache.Remove(key);
+            }
+        }
+
+        public void Set<T>(string key, T data, int cacheTime)
+        {
+            if (IsSet(key))
+            {
+                tmpCache[key] = data;
+            }
+            else
+            {
+                tmpCache.Add(key, data);
+            }
+        }
+
+        public Task<T> GetAsync<T>(string key)
+        {
+            cacheLock.EnterReadLock();
+            try
+            {
+                return Task.FromResult((T)tmpCache[key]);
+            }
+            finally
+            {
+                cacheLock.ExitReadLock();
+            }
+        }
+
+        public async Task SetAsync<T>(string key, T data, int cacheTime)
+        {
+            if (data == null)
+            {
+                await RemoveAsync(key);
+                return;
+            }
+            if (await IsSetAsync(key))
+            {
+                cacheLock.EnterWriteLock();
+                try
+                {
+                    tmpCache[key] = data;
+                }
+                finally
+                {
+                    cacheLock.ExitWriteLock();
+                }
+            }
+            else
+            {
+                cacheLock.EnterWriteLock();
+                try
+                {
+                    tmpCache.Add(key, data);
+                }
+                finally
+                {
+                    cacheLock.ExitWriteLock();
+                }
+            }
+        }
+
+        public Task<bool> IsSetAsync(string key)
+        {
+            cacheLock.EnterReadLock();
+            try
+            {
+                if (tmpCache == null) { return Task.FromResult(false); }
+                if (tmpCache.ContainsKey(key))
+                {
+                    return Task.FromResult(true);
+                }
+                return Task.FromResult(false);
+            }
+            finally
+            {
+                cacheLock.ExitReadLock();
+            }
+        }
+
+        public async Task RemoveAsync(string key)
+        {
+            if (await IsSetAsync(key))
+            {
+                cacheLock.EnterWriteLock();
+                try
+                {
+                    tmpCache.Remove(key);
+                }
+                finally
+                {
+                    cacheLock.ExitWriteLock();
+                }
+            }
+        }
+    }
+}
